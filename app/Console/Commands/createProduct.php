9bfe060971cbe550ab1d86076003a26083758feb
l <viroxic@gmail.com>
2021-03-14 14:21:41 +0100

    create product command

---
+++ app/Console/Commands/createProduct.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class createProduct extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:product';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a new product';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        do {
+            $name = $this->ask('Enter product name :');
+
+            if (Product::whereName($name)->count()) {
+                $this->error('This product already exists!');
+                continue;
+            }
+
+            Product::create([
+                'name' => $name,
+            ]);
+            $this->info($name . ' has been successfully created!');
+
+            return;
+        } while (true);
+    }
+}
