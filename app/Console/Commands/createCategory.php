1fdcb6e0b64baf24b2dc9117b7ec83ec419f8582
l <viroxic@gmail.com>
2021-03-14 14:28:42 +0100

    create a category

---
+++ app/Console/Commands/createCategory.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use Illuminate\Console\Command;
+
+class createCategory extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:category';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a new category';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        do {
+            $name = $this->ask('Enter category name');
+
+            if (Category::whereName($name)->count()) {
+                $this->error('This category already exists!');
+                continue;
+            }
+
+            Category::create([
+                'name' => $name,
+            ]);
+            $this->info($name . ' has been successfully created!');
+
+            return;
+        } while (true);
+    }
+}
