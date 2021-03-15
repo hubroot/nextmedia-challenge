fd1aca3f389e9f5abd6dbca52c5e1c6d88ff45e3
l <viroxic@gmail.com>
2021-03-15 16:24:54 +0100

    attach categories & products to list products component

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col">
-            <list-products></list-products>
+            <list-products :products="{{ $products }}" :categories="{{ $categories }}"></list-products>
         </div>
     </div>
 </div>
